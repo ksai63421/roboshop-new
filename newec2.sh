99a030bae2b8b3709a107838372ea03719f058a0
saikrishna94101 <saikrishna94101@gmail.com>
2024-01-03 06:30:51 +1100

    creating ec2.sh

--- newec2.sh
+++ newec2.sh
@@ -16,7 +16,7 @@ do
     fi   
    echo "creating $i instance"
 
-IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro  --security-group-ids sg-0885f2e62d2bb3c88 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.instances[0], PrivateIpAddress')
+IP_ADDRESS=$(aws ec2 run-instances --image-id ami-03265a0778a880afb --instance-type t2.micro  --security-group-ids sg-0885f2e62d2bb3c88 --tag-specifications "ResourceType=instance,Tags=[{Key=Name,Value=$i}]" | jq -r '.Instances[0], PrivateIpAddress')
 east
 
 echo "created $i instance: $IP_ADDRESS"
