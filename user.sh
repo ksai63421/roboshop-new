8772af359bb601935f6524d42e006f27f2180023
saikrishna94101 <saikrishna94101@gmail.com>
2024-01-02 16:51:20 +1100

    added user

---
+++ user.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+DATE=$(date +%F)
+LOGSDIR=/tmp
+# inside the above folder /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+USERID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+N="\e[0m"
+Y="\e[33m"
+
+if [ $USERID -ne 0 ];
+then
+  echo -e "$R ERROR:: Please run this script with root access $N"
+  exit 1
+fi
+VALIDATE(){
+  if [ $1 -ne 0 ];
+  then 
+     echo -e "$2....$R FAILURE $N"
+     exit 1
+     else 
+      echo -e "$2...$G SUCCESS $N"
+      fi
+}
+
+curl -sL https://rpm.nodesource.com/setup_lts.x | bash &>>$LOGFILE
+
+VALIDATE $? "Setting up NPM source"
+
+yum install nodejs -y &>>$LOGFILE
+
+VALIDATE $? "Installing NodeJS"
+
+# once user is created if you run this script for the 2nd time 
+# this command will fail ?
+# improvement: check the user alredy exit or not if not then create
+useradd roboshop &>>$LOGFILE  
+
+# write a condifiton to check dir alredy exit or not
+
+mkdir /app &>>$LOGFILE
+
+curl -o /tmp/user.zip https://roboshop-builds.s3.amazonaws.com/user.zip &>>$LOGFILE
+
+VALIDATE $? "downloading user artifact"
+
+cd /app &>>$LOGFILE
+
+VALIDATE $? "moving into app dir"
+
+unzip /tmp/user.zip &>>$LOGFILE
+
+VALIDATE $? "unzipping user"
+
+npm install &>>$LOGFILE
+
+VALIDATE $? "installing dependencies"
+
+cp /home/centos/roboshop-new/user.service /etc/systemd/system/user.service &>>$LOGFILE
+
+VALIDATE $? "copying user.service"
+
+systemctl daemon-reload &>>$LOGFILE
+
+VALIDATE $? "daemon reload"
+
+systemctl enable user &>>$LOGFILE
+
+VALIDATE $? "Enabling user"
+
+systemctl start user &>>$LOGFILE
+
+VALIDATE $? "starting user"
+
+cp /home/centos/roboshop-new/mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOGFILE
+
+VALIDATE $? "copying monogo repo"
+
+yum install mongodb-org-shell -y &>>$LOGFILE
+
+VALIDATE $? "installing mongo client"
+
+mongo --host mongodb.joindevops.eu </app/schema/user.js &>>$LOGFILE 
+
+VALIDATE $? "loading user data into mongodb"
